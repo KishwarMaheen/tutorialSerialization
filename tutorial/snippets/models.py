c4eff1df784a3d02d0bd73c1aa32d31ef4100c87
KishwarMaheen <34351287+KishwarMaheen@users.noreply.github.com>
2020-12-03 13:52:36 +0600

    Implemented generic class-based API views.

--- tutorial/snippets/models.py
+++ tutorial/snippets/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-from pygments.lexers import get_all_lexers
+from pygments.lexers import get_all_lexers, get_lexer_by_name
+from pygments.formatters.html import HtmlFormatter
+from pygments import highlight
 from pygments.styles import get_all_styles
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
