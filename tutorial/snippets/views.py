d5cb3c46d451afe4ba3bfef7d0aa131ac2281593
KishwarMaheen <34351287+KishwarMaheen@users.noreply.github.com>
2020-12-03 15:41:10 +0600

    Implemented PATCH request handling for function views, class views and mixin class views.

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -32,7 +32,7 @@ from rest_framework import generics
 #
 #
 # # @csrf_exempt
-# @api_view(['GET', 'PUT', 'DELETE'])
+# @api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
 # def snippet_detail(request, pk, format=None):
 #     try:
 #         snippet = Snippet.objects.get(pk=pk)
@@ -41,7 +41,7 @@ from rest_framework import generics
 #     if request.method == 'GET':
 #         serializer = SnippetSerializer(snippet)
 #         return Response(serializer.data)
-#     elif request.method == 'PUT':
+#     elif request.method == 'PUT' or request.method == 'PATCH':
 #         serializer = SnippetSerializer(snippet, data=request.data)
 #         if serializer.is_valid():
 #             serializer.save()
@@ -64,8 +64,8 @@ from rest_framework import generics
 #             serializer.save()
 #             return Response(serializer.data, status=status.HTTP_201_CREATED)
 #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#
+
+
 # class SnippetDetail(APIView):
 #     def get_object(self, pk):
 #         try:
@@ -73,18 +73,24 @@ from rest_framework import generics
 #         except Snippet.DoesNotExist:
 #             raise Http404
 #
-#     def get(self, request, pk, format=None):
-#         snippet = self.get_object(pk)
-#         serializer = SnippetSerializer(snippet)
-#         return Response(serializer.data)
-#
-#     def put(self, request, pk, format=None):
+#     def update(self, request, pk):
 #         snippet = self.get_object(pk)
 #         serializer = SnippetSerializer(snippet, data=request.data)
 #         if serializer.is_valid():
 #             serializer.save()
 #             return Response(serializer.data)
 #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def get(self, request, pk, format=None):
+#         snippet = self.get_object(pk)
+#         serializer = SnippetSerializer(snippet)
+#         return Response(serializer.data)
+#
+#     def put(self, request, pk, format=None):
+#         return self.update(request, pk)
+#
+#     def patch(self, request, pk, format=None):
+#         return self.update(request, pk)
 #
 #     def delete(self, request, pk, format=None):
 #         snippet = self.get_object(pk)
@@ -101,8 +107,8 @@ from rest_framework import generics
 #
 #     def post(self, request, *args, **kwargs):
 #         return self.create(request, *args, **kwargs)
-#
-#
+
+
 # class SnippetDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin,
 #                     generics.GenericAPIView):
 #     queryset = Snippet.objects.all()
@@ -114,8 +120,12 @@ from rest_framework import generics
 #     def put(self, request, *args, **kwargs):
 #         return self.update(request, *args, **kwargs)
 #
+#     def patch(self, request, *args, **kwargs):
+#         return self.partial_update(request, *args, **kwargs)
+#
 #     def delete(self, request, *args, **kwargs):
 #         return self.destroy(request, *args, **kwargs)
+
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
